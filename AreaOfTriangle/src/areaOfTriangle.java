45b80df361e6040f555c89aef66a832dc646ec84
lodrickthomas <lodrick_official@hotmail.com>
2017-11-04 22:05:52 +0300

    improvement on how to calculate the area of triangle in class areaOfTriangle.class

--- AreaOfTriangle/src/areaOfTriangle.java
+++ AreaOfTriangle/src/areaOfTriangle.java
@@ -1,20 +1,56 @@
+import java.text.DecimalFormat;
 import java.util.*;
 
-public class AreaOfTriangle {
+public class areaOfTriangle {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the length: ");
-		double base = input.nextDouble();
-		System.out.print("Enter the height: ");
-		double height = input.nextDouble();
-		
-		double area = (base * height) / 2;
-		// calculates area using formula (b*h)/2
-		
-		System.out.printf("The area of the triangle is %.2f", area); 
-		// %f cause were using doubles and %.2f to limit to 2 decimal places.
+
+	    //create an object of class AreaOfTriangle...
+        AreaOfTriangle areaoftriageobj = new AreaOfTriangle();
+
+        //store an object in result variable
+        double result = areaoftriageobj.triangle();
+
+        System.out.println("The area of triangle is: " + result + " millimeter square");
+
+        //get results in 2 decimal places using DecimalFormat class...
+        DecimalFormat n = new DecimalFormat("#.##");
+
+        System.out.println("The area of triangle in 2 decimalplaces is: " + n.format(result) + " millimeter square");
+
+        //get results in 4 decimal places using DecimalFormat class...
+        DecimalFormat n1 = new DecimalFormat("#.####");
+
+        System.out.println("The area of triangle in 4 decimalplaces is: " + n1.format(result) + " millimeter square");
+
+
 
 	}
+
+}
+class AreaOfTriangle{
+    double base, height;
+    Scanner input = new Scanner(System.in);
+    public double triangle(){
+        //request the value of length from user...
+        System.out.print("Enter the value base of triangle in millimeter: ");
+        base = input.nextDouble();
+
+        //request the value of height from user...
+        System.out.print("Enter the value height of triangle in millimeter: ");
+        height = input.nextDouble();
+
+        // calculates the area of triangle using formula (b*h)/2
+        double area = (base * height) *0.5;
+
+        return area;
+
+
+//        System.out.printf("The area of the triangle is %.2f", area);
+        // %f cause were using doubles and %.2f to limit to 2 decimal places.
+
+
+    }
+
 
 }
